78d64765867c6080a1fd0c7b2254342238b203ff
Sarthak Kapoor <sarthakkapoor21@gmail.com>
2017-07-13 03:38:20 +0530

    Initial Commit

---
+++ KindaSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindaSocial
+//
+//  Created by Sarthak Kapoor on 13/07/17.
+//  Copyright © 2017 SK21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
